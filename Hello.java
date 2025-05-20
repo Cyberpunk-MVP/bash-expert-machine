26e75759db461bb8866e457da1afac1ff0983c57
Cyberpunk-MVP <mkhizelwazoluhle@outlook.com>
2025-05-20 10:31:45 +0200

    Create Hello.java
    Hello World!

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class HelloWorld { // Declares a public class named HelloWorld
+    public static void main(String[] args) { // This is the main method, the entry point of the program
+        // The line below prints "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
